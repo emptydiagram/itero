1f603c2cf6b7a3676d3644a2d30e07c31d641f16
Nick Hamann <nah0024@auburn.edu>
2020-04-11 17:05:10 -0400

    Introduce Top, Node components

---
+++ src/Node.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let entries, goBack;
+
+</script>
+
+<p>node</p>
+
+<ul>
+{#each entries as entry}
+  <li>{entry}</li>
+{/each}
+</ul>
+
+
+<button on:click={goBack}>go back</button>
---
+++ src/Top.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let displayNodes, createNode, viewNode;
+
+</script>
+
+<p>TOP</p>
+<button on:click={createNode}>create new node</button>
+
+<ul>
+{#each displayNodes as node, i}
+  <li>{node}</li>
+  <button on:click={() => viewNode(i+1)}>view node {i}</button>
+{/each}
+</ul>
+
+
