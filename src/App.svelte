552fe3548f7cfcd0abd30d9c85ec1a352aa63ccc
Nick Hamann <nah0024@auburn.edu>
2020-04-11 17:23:03 -0400

    Refactor machine definition to separate file

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
-  import { Machine, interpret, assign } from 'xstate';
+import { assign, interpret } from 'xstate';
   import Node from './Node.svelte';
   import Top from './Top.svelte';
+  import flowikiMachine from './machine.js';
 
   let currentNodeId;
 
@@ -15,90 +16,16 @@
     };
   });
 
-  let createNodeAction = assign(ctxt => {
-    return {
-      displayNodeEntries: [],
-    };
-  });
+  let machine = flowikiMachine(viewNodeAction);
+  let machineState = machine.initialState;
+  const flowikiService = interpret(machine);
 
-  const flowikiStates = {
-    initial: 'navigating',
-    states: {
-      navigating: {
-        on: {
-          START_EDIT: {
-            target: 'editing'
-          }
-        }
-      },
-
-      editing: {
-        on: {
-          SAVE: {
-            target: 'navigating',
-          }
-        }
-      }
-    }
-  }
-
-  const flowikiMachine = Machine(
-    {
-      id: 'flowiki',
-      initial: 'top',
-      context: {
-        currentNodeId: null,
-        nodes: {
-          '1': {
-            name: 'foo',
-            entries: ['a', 'b', 'c'],
-          },
-          '2': {
-            name: 'bar',
-            entries: ['4', '5' ],
-          },
-          '3': {
-            name: 'baz',
-            entries: ['alpha', 'beta', 'gamma', 'delta']
-          }
-        },
-        displayNodes: [1, 2, 3],
-        displayNodeEntries: [],
-      },
-      states: {
-        top: {
-          on: {
-            VIEW_NODE: {
-              target: 'node',
-              actions: viewNodeAction,
-            },
-            CREATE_NODE: {
-              target: 'node',
-              actions: createNodeAction,
-            },
-          },
-        },
-        node: {
-          on: {
-            BACK: {
-              target: 'top',
-            }
-          },
-          ...flowikiStates
-        }
-      }
-    });
-
-  const flowikiService = interpret(flowikiMachine);
-
-  let machineState = flowikiMachine.initialState;
   flowikiService.onTransition(state => {
     console.log("transitioning to context = ", state.context, ", state = ", state.value);
     machineState = state;
   });
 
   flowikiService.start();
-
 
   //console.log("+++machineState = ", machineState);
   $: isAtTop = machineState.value === 'top';
@@ -131,7 +58,11 @@
 <h1>treacle</h1>
 
 {#if isAtTop}
-  <Top displayNodes={displayNodes} createNode={createNode} viewNode={viewNode} />
+  <Top
+    displayNodes={displayNodes}
+    createNode={createNode}
+    viewNode={viewNode}
+  />
 {:else}
   <Node entries={displayNodeEntries} goBack={goBack} />
 {/if}
