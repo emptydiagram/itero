cb88fa6937cbc3b1c567159a6eab9ad44293024f
Nick Hamann <nah0024@auburn.edu>
2020-04-11 15:31:13 -0400

    WIP: Layout initial high-level machine

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,81 @@
 <script>
+  import { Machine, interpret, assign } from 'xstate';
 	export let name;
+
+  const flowikiStates = {
+    initial: 'navigating',
+    states: {
+      navigating: {
+        on: {
+          /*
+          UP: {
+            actions: assign(ctxt => {
+              console.log('TODO, UP. context = ', ctxt);
+              return {
+                value: newValue,
+                history: [...ctxt.history, newValue],
+              };
+            })
+          },
+           */
+          START_EDIT: {
+            target: 'editing'
+          }
+        }
+      },
+
+      editing: {
+        on: {
+          SAVE: {
+            target: 'navigating',
+          }
+        }
+      }
+    }
+  }
+
+  const flowikiMachine = Machine(
+    {
+      id: 'flowiki',
+      initial: 'top',
+      context: {
+        nodes: {},
+        displayNodes: [],
+        displayNodeEntries: [],
+      },
+      states: {
+        top: {
+          on: {
+            VIEW_NODE: {
+              target: 'node',
+              //actions: initFromRandom
+            },
+            CREATE_NODE: {
+              target: 'node',
+              //actions: initFromText
+            },
+          },
+        },
+        node: {
+          on: {
+            BACK: {
+              target: 'top',
+            }
+          },
+          ...flowikiStates
+        }
+      }
+    });
+
+  const flowikiService = interpret(flowikiMachine);
+
+  let machineState = flowikiMachine.initialState;
+  flowikiService.onTransition(state => {
+    console.log("transitioning to context = ", state.context, ", state = ", state.value);
+    machineState = state;
+  });
+
+  flowikiService.start();
 </script>
 
 <style>
