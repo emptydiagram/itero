a426448687c26f5972ff8d95bd9b97ec43a6b155
llamachannel <llamachannel@fastmail.com>
2020-04-11 21:36:40 -0400

    Implement highlighting / a "go up" button

--- src/Node.svelte
+++ src/Node.svelte
@@ -1,5 +1,13 @@
 <script>
-  export let entries;
+  export let entries, nodeCursorId, goUp;
+
+  $: highlightedEntries = entries.map((entry, i) => {
+    console.log("for i = ", i, ", entry = ", entry);
+    return {
+      entry: entry,
+      highlighted: i === nodeCursorId
+    };
+  });
 
 </script>
 
@@ -8,11 +16,23 @@
     font-family: Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace;
     font-size: 1em;
   }
+
+  .highlighted {
+    background-color: #f3ffff
+  }
 </style>
 
+<p>{nodeCursorId}</p>
+
+<button disabled={nodeCursorId == 0} on:click={goUp}>up</button>
+
 <ul id="entries">
-{#each entries as entry}
-  <li>{entry}</li>
+{#each highlightedEntries as hlEntry, i}
+    {#if hlEntry.highlighted}
+      <li class="highlighted">{hlEntry.entry}</li>
+    {:else}
+      <li>{hlEntry.entry}</li>
+    {/if}
 {/each}
 </ul>
 
