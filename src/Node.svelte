cd1a4131ce241cf04a0a6547ae0293c8b968f887
Nick Hamann <nah0024@auburn.edu>
2020-04-11 20:36:32 -0400

    Style Top

--- src/Node.svelte
+++ src/Node.svelte
@@ -3,9 +3,14 @@
 
 </script>
 
-<p>node</p>
+<style>
+  #entries {
+    font-family: Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace;
+    font-size: 1em;
+  }
+</style>
 
-<ul>
+<ul id="entries">
 {#each entries as entry}
   <li>{entry}</li>
 {/each}
