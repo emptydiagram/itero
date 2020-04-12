33b708758a1bd072f34f503fe70f801fc43d3bef
llamachannel <llamachannel@fastmail.com>
2020-04-11 20:36:32 -0400

    Style Top

--- src/Top.svelte
+++ src/Top.svelte
@@ -3,10 +3,22 @@
 
 </script>
 
-<p>TOP</p>
+<style>
+  #nodesList {
+    border-top: 1px solid #000;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  #nodesList li {
+    padding: 0.5em 0;
+    border-bottom: 1px solid #000;
+  }
+</style>
+
 <button on:click={createNode}>create new node</button>
 
-<ul>
+<ul id="nodesList">
 {#each displayNodes as node}
   <li><a href={"#/"+node.id}>{node.name}</a></li>
 {/each}
