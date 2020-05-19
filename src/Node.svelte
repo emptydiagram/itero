e07545c7721ba852562d3b24e2617d7ef20178f0
ohalmanacs <ohalmanacs@fastmail.com>
2020-05-18 23:46:59 -0400

    Format using svelte-vscode

--- src/Node.svelte
+++ src/Node.svelte
@@ -1,12 +1,22 @@
 <script>
-  export let entries, nodeTitle, nodeCursorRowId, nodeCursorColId, nodeIsEditingName;
-  export let handleStartEditingNodeName, handleCancelEditingNodeName, handleSaveNodeName;
-  export let handleSaveNodeEntry, handleSaveFullCursor, handleGoUp, handleGoDown, handleEntryBackspace, handleSplitEntry;
+  export let entries,
+    nodeTitle,
+    nodeCursorRowId,
+    nodeCursorColId,
+    nodeIsEditingName;
+  export let handleStartEditingNodeName,
+    handleCancelEditingNodeName,
+    handleSaveNodeName;
+  export let handleSaveNodeEntry,
+    handleSaveFullCursor,
+    handleGoUp,
+    handleGoDown,
+    handleEntryBackspace,
+    handleSplitEntry;
   export let handleSaveCursorColId;
-  import EntryInput from './EntryInput.svelte';
+  import EntryInput from "./EntryInput.svelte";
 
   let nodeTitleText = nodeTitle;
-
 
   $: handleSaveName = () => handleSaveNodeName(nodeTitleText);
 
@@ -18,11 +28,10 @@
   $: handleStartEditing = () => {
     nodeTitleText = nodeTitle;
     handleStartEditingNodeName();
-  }
+  };
 
   $: atFirstRow = nodeCursorRowId === 0;
   $: atLastRow = nodeCursorRowId === entries.length - 1;
-
 </script>
 
 <style>
@@ -31,7 +40,7 @@
     text-decoration: underline;
   }
   .node-name-edit-action:active {
-    color: rgb(0,80,160);
+    color: rgb(0, 80, 160);
   }
 
   #node-name {
@@ -62,23 +71,30 @@
     background-color: #f7f7f2;
   }
 
-
   #entries {
-    font-family: Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace;
+    font-family: Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console",
+      "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono",
+      "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier,
+      monospace;
     font-size: 1em;
   }
 
   .highlighted {
     background-color: #e6fcf1;
   }
-
 </style>
 
 {#if nodeIsEditingName}
   <div>
-    <input type="text" id="node-name-input" bind:value={nodeTitleText} placeholder="Document name"/>
+    <input
+      type="text"
+      id="node-name-input"
+      bind:value={nodeTitleText}
+      placeholder="Document name" />
     <span class="node-name-edit-action" on:click={handleSaveName}>save</span>
-    <span class="node-name-edit-action" on:click={handleEditingCancel}>cancel</span>
+    <span class="node-name-edit-action" on:click={handleEditingCancel}>
+      cancel
+    </span>
   </div>
 {:else}
   <div id="node-name">
@@ -88,23 +104,22 @@
 {/if}
 
 <ul id="entries">
-{#each entries as entry, i}
-  <li class={i === nodeCursorRowId ? "highlighted" : ""}>
-    <EntryInput
-      entryId={i}
-      entryValue={entry}
-      nodeCursorRowId={nodeCursorRowId}
-      nodeCursorColId={nodeCursorColId}
-      atFirstRow={atFirstRow}
-      atLastRow={atLastRow}
-      handleSaveNodeEntry={handleSaveNodeEntry}
-      handleSaveFullCursor={handleSaveFullCursor}
-      handleGoUp={handleGoUp}
-      handleGoDown={handleGoDown}
-      handleSplitEntry={handleSplitEntry}
-      handleEntryBackspace={handleEntryBackspace}
-      handleSaveCursorColId={handleSaveCursorColId}
-    />
-  </li>
-{/each}
+  {#each entries as entry, i}
+    <li class={i === nodeCursorRowId ? 'highlighted' : ''}>
+      <EntryInput
+        entryId={i}
+        entryValue={entry}
+        {nodeCursorRowId}
+        {nodeCursorColId}
+        {atFirstRow}
+        {atLastRow}
+        {handleSaveNodeEntry}
+        {handleSaveFullCursor}
+        {handleGoUp}
+        {handleGoDown}
+        {handleSplitEntry}
+        {handleEntryBackspace}
+        {handleSaveCursorColId} />
+    </li>
+  {/each}
 </ul>
