2cf2b2dd6775c9b4b0a8e745e1f69e03d508f702
ohalmanacs <ohalmanacs@fastmail.com>
2020-05-25 16:28:11 -0400

    WIP: add saveDocuments, documentToSerializationObject

--- src/serialization.js
+++ src/serialization.js
@@ -7,22 +7,28 @@ function isObject(obj) {
 
 // returns: a FlowyTreeNode which corresponds to the specification in treeObj
 export function treeObjToNode(treeObj, parentId) {
-    let rootKey = Object.keys(treeObj)[0];
-    let currNode = treeObj[rootKey];
-    let currId = rootKey === "root" ? null : parseInt(rootKey);
-    let nodesArray = Array.from(
-        currNode,
-        child => new LinkedListItem(
-            isObject(child)
-                ? treeObjToNode(child, currId)
-                : new FlowyTreeNode(child, currId)));
-    // a linked list of (LinkedListItems of) FlowyTreeNodes, one for each child in treeObj
-    let nodesList = new LinkedList(...nodesArray);
+  let rootKey = Object.keys(treeObj)[0];
+  let currNode = treeObj[rootKey];
+  let currId = rootKey === "root" ? null : parseInt(rootKey);
+  let nodesArray = Array.from(
+    currNode,
+    child => new LinkedListItem(
+      isObject(child)
+        ? treeObjToNode(child, currId)
+        : new FlowyTreeNode(child, currId)));
+  // a linked list of (LinkedListItems of) FlowyTreeNodes, one for each child in treeObj
+  let nodesList = new LinkedList(...nodesArray);
 
-    parentId = parentId === 0 ? parentId : (parentId || null);
-    return new FlowyTreeNode(currId, parentId, nodesList);
+  parentId = parentId === 0 ? parentId : (parentId || null);
+  return new FlowyTreeNode(currId, parentId, nodesList);
 }
 
 export function nodeToTreeObj(node) {
-    throw 'not yet implemented'
+  if (node.hasChildren()) {
+    let rootKey = node.getId() == null ? 'root' : ''+node.getId();
+    let result = {};
+    result[rootKey] = node.getChildNodeArray().map(item => nodeToTreeObj(item));
+    return result;
+  }
+  return node.getId();
 }
