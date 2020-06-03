1e2cfa531c740e9143c301c8ef4afe99b02ca8e4
llamachannel <llamachannel@fastmail.com>
2020-06-03 00:55:45 -0400

    Fix cursor positioning when clicking into entry with links

---
+++ src/markup/util.js
@@ -0,0 +1,66 @@
+
+// Background: This markup:
+//
+// ```
+// xyz **abc [def](foo) ghi** jkl
+// ```
+//
+// renders to:
+//
+// ```
+//  xyz <strong>abc <a href="foo">def</a> ghi</strong> jkl
+// ```
+//
+// Problem: given renderedEntryNode child nodes below, and a pointer `anchorNode`
+// to some text element that was clicked on, find the appropriate cursor position
+// within the associated markup text
+//
+//  - "xyz "
+//  - <strong>
+//     - "abc "
+//     - <a href="foo">
+//        - "def"
+//     - " ghi"
+//  - " jkl"
+//
+// e.g.: given a pointer to "def", return 11 (preceded by "xyz **abc [")
+// e.g.: given a pointer to " ghi", return 20 (preceded by "xyz **abc [def](foo)")
+//
+// returns { found: boolean, pos: number }
+export function findChildNodeSerializedCursorPosFromSelection(n, sel, pos) {
+  if (n.nodeType !== Node.TEXT_NODE && n.nodeType !== Node.ELEMENT_NODE) {
+    return null;
+  }
+  if (n.nodeType === Node.TEXT_NODE) {
+    if (n === sel.anchorNode) {
+      return { found: true, pos: pos + sel.anchorOffset };
+    } else {
+      return { found: false, pos: pos + n.textContent.length }
+    }
+  }
+
+  if (n.localName === "strong") {
+    pos += 2;
+  } else if (n.localName === "a") {
+    pos += 1;
+  }
+
+  let currNode = n;
+  let childNodes = currNode.childNodes;
+  for(var i = 0; i < childNodes.length; ++i) {
+    let result = findChildNodeSerializedCursorPosFromSelection(childNodes[i], sel, pos);
+    if (result.found) {
+      return result;
+    }
+    pos = result.pos;
+  }
+
+  if (n.localName === "strong") {
+    pos += 2;
+  } else if (n.localName === "a") {
+    // TODO
+    pos += n.getAttribute("href").length + 3;
+  }
+
+  return { found: false, pos: pos };
+}
