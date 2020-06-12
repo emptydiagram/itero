f71d3ad25a00f54a350a66b6772d2eb7bfb20479
ohalmanacs <ohalmanacs@fastmail.com>
2020-06-11 22:11:04 -0400

    Replace some App component state with stores

--- src/stores.js
+++ src/stores.js
@@ -1,3 +1,6 @@
 import { writable } from 'svelte/store';
 
+export const nextDocCursorEntryId = writable(null);
+export const nextDocCursorColId = writable(0);
 export const nextDocName = writable('');
+export const nextDocEntryText = writable('');
