a607acba27ca93591f686a4d813bc6b72181c856
llamachannel <llamachannel@fastmail.com>
2020-07-04 17:44:58 -0400

    Remove unused store methods

--- src/components/stores.js
+++ src/components/stores.js
@@ -69,14 +69,6 @@ function createDocsStore() {
       return store;
     }),
 
-    saveCurrentDocId: (newCurrentDocId) => update(store => {
-      store.currentDocId = newCurrentDocId;
-      return store;
-    }),
-    saveDocName: (newDocName) => update(store => {
-      store.docName = newDocName;
-      return store;
-    }),
     saveCurrentPageDocName: (newDocName) => update(store => {
       // sync page's doc name display
       store.docName = newDocName;
@@ -107,24 +99,7 @@ function createDocsStore() {
       store.cursorColId = newColId;
       return store;
     }),
-    saveCursorEntryId: (newEntryId) => update(store => {
-      store.cursorEntryId = newEntryId;
-      return store;
-    }),
 
-    putDocIdLookup: (docName, docId) => update(store => {
-      store.docIdLookupByDocName[docName] = docId;
-      return store;
-    }),
-    removeDocIdLookup: (docName) => update(store => {
-      delete store.docIdLookupByDocName[docName];
-      return store;
-    }),
-
-    appendToDocsDisplayList: (newDocId) => update(store => {
-      store.docsDisplayList.push(newDocId);
-      return store;
-    }),
     entryGoUp: () => update(store => {
       let currDocId = store.currentDocId;
       let cursorEntryId = store.cursorEntryId;
